f75b4f5da25862f8f620ca3906257c9c2aa95084
parthlambdatest <parthkirsan@lambdatest.com>
2024-11-20 16:15:59 +0530

    dummy commit

--- src/lib/schemaValidation.ts
+++ src/lib/schemaValidation.ts
@@ -150,7 +150,7 @@ const ConfigSchema = {
                     errorMessage: "Invalid config; password is mandatory"
                 },
             }
-        },
+        }, 
         delayedUpload: {
             type: "boolean",
             errorMessage: "Invalid config; delayedUpload must be true/false"
